02b09ea7d6aa5be6bb0aca4ad0751a40eb6d6352
BinarySky10 <yilingxiankong@hotmail.com>
2022-04-11 15:33:36 +0800

    discoverthree场景系统

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,17 +3,24 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import MineSweeper from '@/views/mine-sweeper.vue' // todo 引入组件需要手动配置, 可优化成为自动引用文件夹中的
 import ThreeView from '@/views/three-view.vue'
+import SystemTest from '@/views/system-test.vue'
 const routes: Array<RouteRecordRaw> = [
   // {
   //   path: '/',
   //   name: 'home',
   //   component: MineSweeper,
   // },
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: ThreeView,
+  // },
   {
     path: '/',
     name: 'home',
-    component: ThreeView,
+    component: SystemTest,
   },
+
 ]
 
 const router: Router = createRouter({
--- src/views/three-view.vue
+++ src/views/three-view.vue
@@ -1,6 +1,4 @@
 <script lang="ts" setup>
-import * as path from 'path'
-import { getuid } from 'process'
 import { defineComponent, onMounted, reactive, ref } from 'vue'
 import * as THREE from 'three'
 import type { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
@@ -60,9 +58,15 @@ onMounted(() => {
 </script>
 <template>
   <div id="three-container" />
-  <button @click="handleResetCamera()">重置相机</button>
-  <button @click="handletoJson()">toJson</button>
-  <button @click="handletoTest()">test</button>
+  <button @click="handleResetCamera()">
+    重置相机
+  </button>
+  <button @click="handletoJson()">
+    toJson
+  </button>
+  <button @click="handletoTest()">
+    test
+    </button>
 </template>
 <style lang="scss" scoped>
 #three-container {
